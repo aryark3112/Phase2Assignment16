005cd5c5d3e46190245861c9ff3397d4eda13c9e
aryark <aryark3112@gmail.com>
2023-09-01 15:30:19 +0530

    first commit

---
+++ WebApplication1/WebApplication1/Models/Student.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace WebApplication1.Models
+{
+    [Table("Student")]
+    public class Student
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        [StringLength(50)]
+        public string Title { get; set; }
+        [Required]
+        [StringLength(50)]
+        public string Designation { get; set; }
+        public DateTime DueDate { get; set; }
+    }
+}
